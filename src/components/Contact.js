5ad851cf8e32ae3074ce072320b772cdec377a2d
Rupal1183 <rupalsonje0107@gamil.com>
2020-08-24 19:15:44 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FaEnvelope, FaLinkedin, FaGithub, FaHackerrank, FaInstagram} from "react-icons/fa";
+
+const Contact = ({contactEmail, contactPara, socialLinks})=>{
+
+    return (
+      <div className="contact_container">
+        <h1>Get in touch.</h1>
+        <p>{contactPara}</p>
+        <br></br>
+        <a className="email_link" href={`mailto:${contactEmail}`}>
+          {contactEmail}
+        </a>
+        <div className="social_links">
+          <ul>
+            <li>
+              <a href={`mailto:${contactEmail}`}>
+                <FaEnvelope></FaEnvelope>
+              </a>
+            </li>
+            <li>
+              <a href={socialLinks[0].url}>
+                <FaLinkedin></FaLinkedin>
+              </a>
+            </li>
+            <li>
+              <a href={socialLinks[1].url}>
+                <FaGithub></FaGithub>
+              </a>
+            </li>
+            <li>
+              <a href={socialLinks[2].url}>
+                <FaHackerrank></FaHackerrank>
+              </a>
+            </li>
+            <li>
+              <a href={socialLinks[3].url}>
+                <FaInstagram></FaInstagram>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+}
+
+export default Contact
---
+++ src/components/yourdata.js
@@ -0,0 +1,99 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+
+  //Change The Website Template
+  name: "rupalsonje",
+
+  //Contact Email
+  contactEmail: "rupalsonje0107@gmail.com",
+
+  // Landing Page Name
+  landingPageName: "Rupal Sonje",
+
+  // Landing Page Paragraph
+  landingPagePara:
+    "I'm a Full Stack Developer living in Kalyan, Maharashtra.",
+    // I work as the Creative Director at WORK in India, while also taking on freelance projects that pique my interest.
+  // Landing Page Image (replace - root/public/assets/photo.jpg)
+  landingPageImage: "assets/rupal.jpg",
+
+  // Change Projects Here
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Face Identification App", //Project Title - Add Your Project Title Here
+      // Image Size Should be (300X300 px)
+      imageSrc: "assets/one.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "https://faceidentification.herokuapp.com/"
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Two",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Three",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    // {
+    //   id: 4, //DO NOT CHANGE THIS (Please)😅
+    //   title: "Project Four",
+    //   imageSrc: "https://via.placeholder.com/300",
+    //   url: "http://chetanverma.com/"
+    // },
+    // {
+    //   id: 5, //DO NOT CHANGE THIS (Please)😅
+    //   title: "Project Five",
+    //   imageSrc: "https://via.placeholder.com/300",
+    //   url: "http://chetanverma.com/"
+    // },
+    // {
+    //   id: 6, //DO NOT CHANGE THIS (Please)😅
+    //   title: "Project Six",
+    //   imageSrc: "https://via.placeholder.com/300",
+    //   url: "http://chetanverma.com/"
+    // }
+    // Paste more projects here from the template
+
+    /*
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 7,
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+  ],
+
+  // Contact Section
+  contactPara:
+    "I am available for freelance and looking for three months internship as front-end or full stack developer in Mumbai city. You can find me on Linkedin and Instagram, download my resume or contact me through email.",
+  social: [
+    // Add Or Remove The Link Accordingly
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/rupal-sonje-037aa418b/"
+    },
+    {
+      name: "Github",
+      url: "https://github.com/rupalsonje"
+    },
+    {
+      name: "Hackerrank",
+      url: "https://www.hackerrank.com/rupalsonje0107"
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/rupal_1183/"
+    }
+  ],
+
+  // About Page
+  aboutTitle: "I make remarkable digital products",
+  aboutPara:"I'm third year engineering student at Ramrao Adik Institute of Technology, Navi Mumbai. I'm passionate about designing fluid digital experiences. In my free time I enjoy watching football games and web-series, playing video games. I spend most of college time working on academic projects. Also, I'm a leading member of technical committee of my college."
+};
